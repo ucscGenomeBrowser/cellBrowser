0458766be5588c808200d4f1a173960e6a57ab6d
Marc Perry <marc.perry@alumni.utoronto.ca>
2024-07-18 08:46:40 -0400

    Add files via upload

--- ucsc/tabulate_cell_types.pl
+++ ucsc/tabulate_cell_types.pl
@@ -3,11 +3,67 @@
 # 
 # 
 # 
-# Last Updated: 2024-07-11, Status: in development
+# Last Updated: 2024-07-18, Status: in development
 
 use strict;
 use warnings;
 use JSON;
+
+=head1 NAME
+
+tabulate_cell_types.pl
+
+=head2 INPUT: 
+
+dataset.json a file required to run the UCSC Cell Browser, it contains an aggregate
+of all kinds of information from each dataset that has been built
+on the alpha, or test (or dev) server for the data wranglers.
+This file lives here:
+/usr/local/apache/htdocs-cells/dataset.json
+Currently, in this version of the Perl script, the path to this file is hardcoded.
+
+=head2 OUTPUT:
+
+One TSV table: "cell-type_table.tsv"
+
+As the subroutines in this script convert the information stored into the top-level
+dataset.json file into a list of paths to the desired dataset.json files inside each 
+dataset on the test server, the script prints information to STDOUT:
+
+The name of the dataset currently being processed,
+
+The name of the relevant cluster field in the meta.tsv file
+
+The name of the different cell-types being processed in the meta.tsv file
+
+I used this information during development to determine if the parsing algorithm
+was working as intended.  I suggest when you run this script you direct that STDOUT
+data to a log file (for reference/troubleshooting/debugging).  The script will create
+hardcoded file name for it's primary output.
+
+=head3 NOTES:  
+
+The code never queries or parses the cellbrowser.conf files, nor does it query or parse the
+meta.tsv files.  Instead, the script parses the requisite dataset.json files in each 
+hierarchical level of the apache server directory.
+
+The main subroutine is called recursively to drill down into the datasets and collections.
+When it reaches a dataset.json file that does not contain a JSON key named 'datasets', it 
+methodically queries each of the three different JSON keys that might store the information
+about which field was used for labelling the clusters.  Once it has found that field, it 
+continues to extract the names of the "cell-types" stored in appropriate metaField.
+
+Once that information is found, it is stored in a global hash (dictionary), along with the
+name of the dataset that used that particular string as a cell-type.
+
+After processing all of the datasets the script iterates over the hash data structure and
+prints out each key on a separate line.  The second column contains a CSV list of the 
+all the datasets that used the field.
+
+
+=cut
+
+my $USAGE = "USAGE:\n $0 > logfile.txt\n";
 
 # N.B., this is a global hash data structure
 # that gets built up or filled in as the various dataset.json
@@ -56,10 +112,11 @@ sub iterate_over_list_of_names {
     } # close main foreach loop
 } # close sub
 
-open my ($FH), '>', 'this_is_the_new_cell-type_table.tsv' or die "Could not open file for writing: $!";
+open my ($FH), '>', 'cell-type_table.tsv' or die "Could not open file for writing: $!";
 
+print $FH "cell_type\tdataset_count\tdataset_names\n";
 foreach my $cell_type ( sort keys %cell_types ) {
-    print $FH "$cell_type\t", join(",", @{$cell_types{$cell_type}} ), "\n";
+    print $FH "$cell_type\t", scalar(@{$cell_types{$cell_type}}), "\t", join(",", @{$cell_types{$cell_type}} ), "\n";
 } # close foreach loop
 
 close $FH;
@@ -77,13 +134,18 @@ sub extract_cell_types {
     if ( $cluster ) {
         print "\t\$cluster contains: $cluster\n";
         foreach my $field ( @{$dataset{metaFields}} ) {
-            if ( $field->{name} eq $cluster ) {
+            if ( $field->{label} eq $cluster ) {
                 if (exists $field->{valCounts} ) {
                     my @values = @{ $field->{valCounts} };
                     foreach my $value ( @values ) {
                         my $cell_type = $value->[0];
-                        print "\t\t\$cell_type contains: $cell_type\n";
-                        push( @{$cell_types{$cell_type}}, $dataset_name, ); 
+                        if ( $cell_type ) {
+                            print "\t\t\$cell_type contains: $cell_type\n";
+                            push( @{$cell_types{$cell_type}}, $dataset_name, ); 
+                        }
+                        else {
+                            next;
+                        }
                     } # close inner foreach loop
                 }
                 else {
