8bd10e85f677fa138f24cf23572662277496e3fe
Christopher Lee <chmalee@ucsc.edu>
2025-01-27 15:54:53 -0800

    New versioneer for new python

--- src/cbPyLib/cellbrowser/__init__.py
+++ src/cbPyLib/cellbrowser/__init__.py
@@ -1,5 +1,4 @@
 # empty init, nothing to do here
 
-from ._version import get_versions
-__version__ = get_versions()['version']
-del get_versions
+from . import _version
+__version__ = _version.get_versions()['version']
