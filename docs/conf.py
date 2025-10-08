18dd6ea0ae6c64a5ded831b520e046eabe389cbf
Max <max@soe.ucsc.edu>
2025-10-08 06:13:10 -0700

    next try

--- docs/conf.py
+++ docs/conf.py
@@ -49,7 +49,8 @@ pygments_style = 'sphinx'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx.ext.viewcode',
+    'sphinx_rtd_theme',
+    'sphinx.ext.viewcode'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
