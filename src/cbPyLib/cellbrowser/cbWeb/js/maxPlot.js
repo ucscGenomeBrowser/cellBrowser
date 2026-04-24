4b60ae9a313c51240ac34151bbe1e5008ba70c9d
Max <max@soe.ucsc.edu>
2026-04-24 06:31:49 -0700

    Adding back removal of hidden coords when calculating center for label placement, we will need to understand one day why Jacob removed this step for webGl.

--- src/cbPyLib/cellbrowser/cbWeb/js/maxPlot.js
+++ src/cbPyLib/cellbrowser/cbWeb/js/maxPlot.js
@@ -4370,7 +4370,7 @@ function MaxPlot(div, top, left, width, height, args) {
             var x = coords[i * 2];
             var y = coords[i * 2 + 1];
 
-            if (isHidden(x, y, i) && !this.usesWebGL())
+            if (isHidden(x, y, i))
                 continue;
 
             calc[label][0].push(x);
