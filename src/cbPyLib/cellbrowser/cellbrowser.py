029b28bbe33b22b901871c4d8c3c05bf60a0f2bf
Matt Speir <mspeir@soe.ucsc.edu>
2026-04-26 18:24:51 -0700

    fixing onlyLower behavior so that it actually stops us from building datasets with uppercase letters in the name, refs #32013

--- src/cbPyLib/cellbrowser/cellbrowser.py
+++ src/cbPyLib/cellbrowser/cellbrowser.py
@@ -5886,8 +5886,11 @@ def rebuildFlatIndex(outDir):
 
 def checkDsCase(inConfFname, relPath, inConfig):
     """ relPath should not be uppercase for top-level datasets at UCSC, as we use the hostname part """
+    # grandfathered datasets that predate the onlyLower enforcement
+    legacyUpperNames = {"adultPancreas"}
     if not "/" in relPath and getConfig("onlyLower", False) and \
-            "name" in inConfig and inConfig["name"].isupper():
+            "name" in inConfig and inConfig["name"] != inConfig["name"].lower() and \
+            inConfig["name"] not in legacyUpperNames:
         errAbort("dataset name or directory name should not contain uppercase characters, as these do not work "
                 "if the dataset name is specified in the URL hostname itself (e.g. cortex-dev.cells.ucsc.edu)")
 
